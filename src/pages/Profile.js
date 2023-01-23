72991afef502d6db08487d8255e36db83716044f
hkhoa-ng <nhkhoa020400@gmail.com>
2023-01-23 13:53:24 +0200

    Finish Profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,19 @@
 import React from 'react';
-import { Box, Heading, HStack, Center, Image, Flex } from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  HStack,
+  Center,
+  Image,
+  Flex,
+  Spacer,
+  Button,
+  Text,
+  VStack,
+  IconButton,
+} from '@chakra-ui/react';
+import { FaRegEdit } from 'react-icons/fa';
+
 import Calendar from 'react-calendar';
 import '../styles/Calendar.css';
 
@@ -7,49 +21,98 @@ import Navbar from '../components/navbar/Navbar';
 
 function Profile() {
   return (
-    <Box bg="gray.900">
+    <Center flexDir="column">
       <Navbar username={'hkhoa'} name="Khoa Nguyen" />
-      <Center>
-        <Box my={'3em'} position="relative">
+
+      {/* Avatar and background image */}
+      <Center my={'3em'} position="relative">
+        {/* Background image */}
+        <Image
+          borderRadius={'10px'}
+          maxH="25vh"
+          w={{ base: '100vw', md: '80vw', xl: '70vw' }}
+          src="https://images5.alphacoders.com/852/852883.jpg"
+        />
+
+        {/* Avatar and name */}
+        <Center
+          position={'absolute'}
+          left={{ base: '50%', md: '0' }}
+          bottom="0"
+          transform={{
+            base: 'translate(-50%, 65%)',
+            md: 'translate(10%, 80%)',
+          }}
+          flexDir={{ base: 'column', md: 'row' }}
+          w="85%"
+        >
+          {/* Profile picture */}
           <Image
-            borderRadius={'10px'}
-            maxH="50vh"
-            src="https://images5.alphacoders.com/852/852883.jpg"
+            borderRadius="50%"
+            w={{ base: '100px', md: '170px' }}
+            src={'https://avatars2.githubusercontent.com/u/37842853?v=4'}
+            border="5px solid #171923"
           />
-          <Center
-            position={'absolute'}
-            left={{ base: '50%', md: '0' }}
-            bottom="0"
-            transform={{
-              base: 'translate(-50%, 55%)',
-              md: 'translate(10%, 70%)',
-            }}
-            flexDir={{ base: 'column', md: 'row' }}
+
+          {/* Profile name + friends button group */}
+          <VStack
+            ml={{ base: 0, md: 7 }}
+            alignItems={{ base: 'center', md: 'flex-start' }}
+            // bg="teal"
+            w="100%"
           >
-            <Image
-              borderRadius="50%"
-              w="170px"
-              src={'https://avatars2.githubusercontent.com/u/37842853?v=4'}
-              border="5px solid #171923"
-            />
-            <Heading ml={{ base: 0, md: 7 }}>Khoa Nguyen</Heading>
-          </Center>
+            <HStack>
+              <Heading fontSize={{ base: '1.5rem', md: '2rem' }}>
+                Khoa Nguyen
+              </Heading>
+              <IconButton
+                variant="ghost"
+                aria-label="Edit Profile"
+                icon={<FaRegEdit />}
+              />
+            </HStack>
+            <HStack>
+              <Text>15 friends</Text>
+              <Spacer />
+              <Button>All Friends</Button>
+            </HStack>
+          </VStack>
+        </Center>
+      </Center>
+
+      {/* Content */}
+      <Flex
+        w={{ base: '100vw', md: '80vw', xl: '70vw' }}
+        mt={{ base: '100px', md: '120px' }}
+        flexDir={{ base: 'column', lg: 'row' }}
+        alignItems={{ base: 'center', lg: 'flex-start' }}
+        gap={{ base: '10px', lg: '0px' }}
+      >
+        <Box w={{ base: '80%', lg: '40%' }}>
+          <Calendar locale="en-GB" />
         </Box>
-      </Center>
-      <Center w="100%" mt={'7rem'}>
-        <HStack gap={10} flexDir={{ base: 'column', md: 'row' }} w="95%">
-          <Box w={{ base: '80%', md: '60%' }}>
-            <Calendar locale="en-GB" />
-          </Box>
-          <Box w={{ base: '50%', md: '30%' }} h="100%" bg="teal">
-            <Heading>Personal</Heading>
-          </Box>
-          <Box w="20%" h="100%" bg="teal">
-            <Heading>Personal</Heading>
-          </Box>
-        </HStack>
-      </Center>
-    </Box>
+        <Spacer />
+        <Box h="100%" w={{ base: '80%', lg: '55%' }}>
+          <VStack gap="0.5em">
+            <Heading
+              w="100%"
+              textAlign={{ base: 'center', lg: 'left' }}
+              fontSize={{ base: '1.5rem', lg: '2rem' }}
+            >
+              Your day at a glance
+            </Heading>
+            <Text textAlign="left">
+              Anim dolor dolore irure qui tempor. Est tempor ipsum consequat
+              magna elit dolor sunt id veniam do dolor aliqua.
+            </Text>
+            <Text textAlign="left">
+              Nisi cupidatat aute dolore reprehenderit minim nostrud cillum nisi
+              duis. Ea ea dolore consectetur commodo ad ipsum deserunt.
+            </Text>
+          </VStack>
+        </Box>
+      </Flex>
+    </Center>
   );
 }
 
