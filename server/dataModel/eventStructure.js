d817c994f626bcd781eef3ab314631ae71e98bc6
hkhoa-ng <nhkhoa020400@gmail.com>
2023-01-25 17:58:49 +0200

    Split files into frontend and backend

---
+++ server/dataModel/eventStructure.js
@@ -0,0 +1,28 @@
+const newEvent = {
+  // Required
+  name: "Random Event Name: This can be long or short",
+  date: "25/1/2022",
+  time: "12:00:00 AM",
+  location: "Hervanta Campus",
+  organizer: "Big boi club",
+
+  // Optional
+  image: "image URL",
+  tags: ["music", "nigh time"],
+  descriptions: ["Paragraph 1", "Paragraph 2"],
+  ticket: [
+    {
+      type: "adult",
+      price: 10,
+      description: "For adults!",
+    },
+    {
+      type: "children",
+      price: 0,
+      description: "For children!",
+    },
+  ],
+
+  // Auto generate
+  id: "w0Ws9s08Mp723",
+};
---
+++ server/dataModel/newUserStructure.js
@@ -0,0 +1,21 @@
+const newUser = {
+  // Required:
+  username: "nhkhoa",
+  password: "hashed password",
+  fullName: "Khoa Nguyen",
+  email: "khoa.nguyen@example.com",
+  phoneNum: "(+358) 123456789",
+
+  // Not required. True by default:
+  emailAlert: true,
+  textAlert: true,
+  webNotification: true,
+  emailPromotion: true,
+
+  // Not required. Can change/update later in settings:
+  address: "Streetkatu 10, Tampere",
+  paymentCard: "1234 4321 2340 2123 2131",
+
+  // Auto generate:
+  userId: "13423iSw0wdW0s",
+};
