2033ebf2b825c858c1c919c5ebf09a6a3c05dc21
hkhoa-ng <nhkhoa020400@gmail.com>
2023-01-28 13:55:03 +0200

    Add details of event organizers

---
+++ server/dataModel/organizerStructure.js
@@ -0,0 +1,12 @@
+const organizer = {
+  // Required:
+  name: "Cool organizer!",
+  description: "Hey! I made cool events!",
+  tags: ["music", "nigh time", "educational"],
+  email: "organizer@example.com",
+  events: ["ID of event 1", "ID of event 2", "..."],
+
+  // Not required
+  image: "image URL",
+  website: "organizer.website.com",
+};
