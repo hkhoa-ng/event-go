a9917466399912339a45d15f140e1b92ec3497d1
An Nguyen <nhatan020902@gmail.com>
2023-02-14 03:57:41 +0700

    ignore build folder and fix stripe payment

--- client/src/components/stripeCheckoutButton/stripeCheckoutButton.js
+++ client/src/components/stripeCheckoutButton/stripeCheckoutButton.js
@@ -6,7 +6,7 @@ const StripeCheckoutButton = ({ price }) => {
     "pk_test_51KSjpCBYp3P0VPu4qlVoSK2SwYMcS0HMwJmI8bPOkJFGDtxyrgYvz2EZNKBeb8n916k41ucT3Wu9qKI3q3QWpklJ00khAWF3Xz";
   const onToken = (token) => {
     axios({
-      url: "payment",
+      url: "http://localhost:1000/payment",
       method: "post",
       data: {
         amount: priceForStripe,
---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require('cors')
+//const compression = require("compression");
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const app = express();
+const port = process.env.PORT || 1000;
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(compression);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+}
+
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log("Sever running on port " + port);
+});
+
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd",
+  };
+
+  stripe.charges.create(body, (stripeError, stripeRes) => {
+    if (stripeError) {
+      res.status(500).send({ error: stripeError });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
+});
