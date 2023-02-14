aa03647535af91a44f8ae8ea616522ff2ebbf1d3
An Nguyen <nhatan020902@gmail.com>
2023-02-15 05:57:04 +0700

    rebase to main

--- client/src/components/stripeCheckoutButton/stripeCheckoutButton.js
+++ client/src/components/stripeCheckoutButton/stripeCheckoutButton.js
@@ -7,8 +7,8 @@ const StripeCheckoutButton = ({ price }) => {
     'pk_test_51KSjpCBYp3P0VPu4qlVoSK2SwYMcS0HMwJmI8bPOkJFGDtxyrgYvz2EZNKBeb8n916k41ucT3Wu9qKI3q3QWpklJ00khAWF3Xz';
   const onToken = token => {
     axios({
-      url: 'payment',
-      method: 'post',
+      url: "http://localhost:1000/payment",
+      method: "post",
       data: {
         amount: priceForStripe,
         token: token,
